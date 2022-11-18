eb0927e5805a994427475455f614744082add030
ElenaBeliaeva1 <elena.belyaeva2003@mail.ru>
2022-11-18 14:56:32 +0300

    Add swiper

--- scripts/services.js
+++ scripts/services.js
@@ -1,3 +1,4 @@
 //Инициализация swiper
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
-new Swiper('.brand-container');
+
+const swiper = new Swiper('.swiper-slide');
